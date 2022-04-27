1e9a361bc391f6c1d052e5ea77b27f6c986f48eb
Kévin Calmant <kevin.calmant@outlook.com>
2022-04-27 23:47:45 +0200

    dev: add summary

--- src/components/VictoryModal.vue
+++ src/components/VictoryModal.vue
@@ -8,7 +8,21 @@
       <div id="panel-fenetre-header">
         <h2 id="panel-fenetre-title">{{ title }}</h2>
         <div>
-          <a href="#" id="panel-fenetre-bouton-fermeture" @click="closeModal()"> X </a>
+          <a href="#" id="panel-fenetre-bouton-fermeture" @click="closeModal()">
+            X
+          </a>
+        </div>
+      </div>
+      <div class="panel-fenetre-contenu">
+        <p class="fin-de-partie-panel-phrase">{{ text }}</p>
+        <div id="fin-de-partie-panel-resume">
+          <div class="fin-de-partie-panel-resume-ligne" v-for="guesses in allGuesses">
+            <Fragment v-for="guess in guesses">
+              <span v-if="guess.isRightPosition" class="emoji-carre-rouge">🟥</span>
+              <span v-else-if="guess.isCorrect" class="emoji-cercle-jaune">🟡</span>
+              <span v-else class="emoji-carre-bleu">🟦</span>
+            </Fragment>
+          </div>
         </div>
       </div>
     </div>
@@ -28,10 +42,18 @@ export default {
   },
   computed: {
     title() {
-      return this.gameStore.victory ? 'Félications' : 'Dommage !';
+      return this.gameStore.victory ? "Félications" : "Dommage";
+    },
+    text() {
+      return this.gameStore.victory
+        ? "Bravo, tu as gagné. Reviens demain pour une nouvelle grille."
+        : "Bien essayé, Reviens demain pour une nouvelle grille.";
     },
     modalToggle() {
       return this.gameStore.modalToggle;
+    },
+    allGuesses() {
+      return this.gameStore.guesses;
     },
   },
   methods: {
@@ -44,4 +66,16 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+#fin-de-partie-panel-resume {
+  display: flex;
+  flex-direction: column;
+  margin-left: 40%;
+}
+
+.fin-de-partie-panel-resume-ligne {
+  display: flex;
+  flex-direction: row;
+  gap: 0.2em;
+}
+</style>
